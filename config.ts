b26c8d7a7503839f4582701b06bc181269bd6f4d
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 10:32:46 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
+  url: "https://isa.ir/",
+  match: "https://isa.ir/**",
+  maxPagesToCrawl: 999999999,
+  outputFileName: "isa.json",
   maxTokens: 2000000,
   selector: "body"
 };
