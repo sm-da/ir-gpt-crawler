7cff57fc6ea8f01f2f9f442b46b34417d6723161
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 09:54:39 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,8 +3,8 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://president.ir/en/",
   match: "https://president.ir/en/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
+  maxPagesToCrawl: 999999999,
+  outputFileName: "president-ir.json",
   maxTokens: 2000000,
   selector: "body",
 };
