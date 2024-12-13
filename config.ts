14faa31f636e810bf044021da5b379318481012f
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 10:10:15 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
+  url: "https://www.sabteahval.ir/",
+  match: "https://www.sabteahval.ir/**",
+  maxPagesToCrawl: 999999999,
+  outputFileName: "sabteahval.json",
   maxTokens: 2000000,
   selector: "body"
 };
