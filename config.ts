068be0ed2d6e1c0208e9d63779e5d5625206f474
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 10:37:21 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://www.cao.ir/",
   match: "https://www.cao.ir/**",
-  maxPagesToCrawl: 999999999,
+  maxPagesToCrawl: 99999999,
   outputFileName: "cao.json",
   maxTokens: 2000000,
   selector: "body",
