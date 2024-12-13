3dc738a3f0612483a0b2936302d2807eca8349dc
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 10:14:50 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://mfa.gov.ir/",
   match: "https://mfa.gov.ir/**",
-  maxPagesToCrawl: 999999999,
+  maxPagesToCrawl: 99999999,
   outputFileName: "output.json",
   maxTokens: 2000000,
   selector: "body",
