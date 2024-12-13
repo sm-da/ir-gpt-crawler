0a2462f8f692f0277846fb792643b933f17db115
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 08:35:52 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://irangov.ir/en",
   match: "https://irangov.ir/en/**",
-  maxPagesToCrawl: 9999999909,
+  maxPagesToCrawl: 999999909,
   outputFileName: "iran-gov.json",
   maxTokens: 2000000,
 };
