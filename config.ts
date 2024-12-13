4acad4ce3bd4addbd96402b794028494544cb6d6
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 10:00:06 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
+  url: "https://mfa.gov.ir/",
+  match: "https://mfa.gov.ir/**",
+  maxPagesToCrawl: 999999999,
   outputFileName: "output.json",
   maxTokens: 2000000,
   selector: "body"
