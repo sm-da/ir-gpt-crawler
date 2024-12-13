1f8b704ea699ee4727db09e84846e903a3e7d20c
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 10:19:47 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
+  url: "https://cloudguard.ir/",
+  match: "https://cloudguard.ir/**",
+  maxPagesToCrawl: 999999999,
+  outputFileName: "cloudguard-ir.json",
   maxTokens: 2000000,
   selector: "body"
 };
