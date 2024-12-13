9e9cd64a6955a2ea57320c203af3a2e9df131053
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 09:52:47 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://www.leader.ir/en",
   match: "https://www.leader.ir/en/**",
-  maxPagesToCrawl: 999999999,
+  maxPagesToCrawl: 99999999,
   outputFileName: "leader.json",
   maxTokens: 2000000,
   selector: "body",
