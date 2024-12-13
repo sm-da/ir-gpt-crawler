a16c64d5ff4e2d20d10cba0d71b2f148e454f9a0
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 09:58:52 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "http://en.parliran.ir/",
   match: "http://en.parliran.ir/**",
-  maxPagesToCrawl: 9999999999,
+  maxPagesToCrawl: 999999999,
   outputFileName: "parliran-ir.json",
   maxTokens: 2000000,
   selector: "body",
